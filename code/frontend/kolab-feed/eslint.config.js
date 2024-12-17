b8b2b6aba390b4a27371b8cd5ba89073029b8258
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-17 16:08:11 -0300

    chore(lint): disable explicit any type rule

--- code/frontend/kolab-feed/eslint.config.js
+++ code/frontend/kolab-feed/eslint.config.js
@@ -23,7 +23,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'react/react-in-jsx-scope': 'off'
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
     },
   },
 )
