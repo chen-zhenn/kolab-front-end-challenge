6ab68afe1e52a86e180a7bb94ab6e34e5d748dd3
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-17 21:09:03 -0300

    test(usecase): add test for data and error handling in Post Use Case

--- code/frontend/kolab-feed/src/main/usecases/Post.spec.ts
+++ code/frontend/kolab-feed/src/main/usecases/Post.spec.ts
@@ -2,9 +2,12 @@ import {
     describe, 
     it, 
     expect,
+    vi,
  } from 'vitest'
 
- import Post from './Post'
+import { IPost } from '../../domain/models'
+import Post from './Post'
+
 
 describe('Use Case Post', () => {
 
@@ -21,17 +24,49 @@ describe('Use Case Post', () => {
     })
 
     describe('GetPosts', () => {
+        const post = Post()
 
         describe('Smoke Test', () => {
 
             it('Should return an getAll method', () => {
-                const post = Post()
                 expect(post).toHaveProperty('getAll')
             })
 
             it('Should check if getAll is a function', () => {
-                const post = Post()
                 expect(typeof post.getAll).toBe('function')
+            })
+        })
+
+        describe('Data', () => {
+            
+            it('should return an empty array', async () => {
+                const mockData: IPost[] = []
+                post.getAll = vi.fn().mockResolvedValue(mockData)
+                const result = await post.getAll()
+                expect(result).toEqual(mockData)
+            })
+
+            it('should return an array of Post', async () => {
+                const mockData: IPost[] = [{
+                    userId: 1,
+                    id: 1,
+                    title: 'Post 1',
+                    body: ''
+
+                }]
+                post.getAll = vi.fn().mockResolvedValue(mockData)
+                const result = await post.getAll()
+                expect(result).toEqual(mockData)
+            })
+        })
+
+        describe('Error Handling', () => {
+
+            it('should throw an error', async () => {
+                post.getAll = vi.fn().mockImplementationOnce(() => {
+                    return Promise.reject(new Error())
+                })
+                await expect(post.getAll()).rejects.toThrow()
             })
         })
     })
