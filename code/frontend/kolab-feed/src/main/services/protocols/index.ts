073184173e7c385465b843dac19c534b4c7fc527
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-17 23:27:00 -0300

    feat(service): implement get Post service

---
+++ code/frontend/kolab-feed/src/main/services/protocols/index.ts
@@ -0,0 +1,4 @@
+export enum HttpStatusMessages {
+    servererror = 'Erro interno: Problema ao processar sua solicitação. Tente novamente mais tarde',
+    badrequest = 'Dados inválidos: Os parâmetros fornecidos na sua solicitação são inválidos.',
+}
