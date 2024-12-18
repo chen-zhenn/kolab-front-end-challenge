3397e45e03f2544bbbf005295318ee1ef40563dc
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-18 13:14:20 -0300

    refactor: split Use Case files by responsability

---
+++ code/frontend/kolab-feed/src/main/usecases/UseCaseGetAllPost.ts
@@ -0,0 +1,37 @@
+import { 
+    IPost, 
+} from '@/domain/models'
+
+import { 
+    IHttpParams, 
+    IHttpResponse,
+    AxiosHttpClient,
+    HttpClient,
+} from '@/infra'
+
+import { 
+    ServiceGetPost, 
+} from '@/main/services'
+
+const { 
+    VITE_API_BASE_URL, 
+} = import.meta.env
+
+
+export async function useCaseGetAllPost(): Promise<IHttpResponse<IPost[]>> {
+
+    const params: IHttpParams = {
+        baseURL: VITE_API_BASE_URL,
+        url: '/posts',
+    }
+
+    try {
+        const httpInstance = AxiosHttpClient.getInstance(params)
+        const httpClient = new HttpClient<IPost[]>(httpInstance.httpClient)
+
+        const serviceGetPost = new ServiceGetPost(params, httpClient)
+        return await serviceGetPost.get() 
+    } catch (error) {
+        throw new Error(`${error}`)
+    }
+}
--- code/frontend/kolab-feed/src/main/usecases/UseCasePost.ts
+++ code/frontend/kolab-feed/src/main/usecases/UseCasePost.ts
@@ -1,43 +1,10 @@
 import { 
-    IPost, 
-} from '@/domain/models'
-
-import { 
-    IHttpParams, 
-    IHttpResponse,
-    AxiosHttpClient,
-    HttpClient,
-} from '@/infra'
-
-import { 
-    ServiceGetPost, 
-} from '@/main/services'
-
-const { 
-    VITE_API_BASE_URL, 
-} = import.meta.env
+    useCaseGetAllPost, 
+} from '@/main/usecases'
 
 export function makePost() {
 
-    async function getAll(): Promise<IHttpResponse<IPost[]>> {
-
-        const params: IHttpParams = {
-            baseURL: VITE_API_BASE_URL,
-            url: '/posts',
-        }
-
-        try {
-            const httpInstance = AxiosHttpClient.getInstance(params)
-            const httpClient = new HttpClient<IPost[]>(httpInstance.httpClient)
-    
-            const serviceGetPost = new ServiceGetPost(params, httpClient)
-            return await serviceGetPost.get() 
-        } catch (error) {
-            throw new Error(`${error}`)
-        }
-    }
-
     return {
-        getAll,
+        getAll: useCaseGetAllPost,
     }
 }
