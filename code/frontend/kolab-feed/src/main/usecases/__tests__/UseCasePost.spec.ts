c789f10b1b34a4b8b472cefdfe37d8ff0f3fab03
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-18 13:14:20 -0300

    refactor: split Use Case files by responsability

--- code/frontend/kolab-feed/src/main/usecases/__tests__/UseCasePost.spec.ts
+++ code/frontend/kolab-feed/src/main/usecases/__tests__/UseCasePost.spec.ts
@@ -2,10 +2,8 @@ import {
     describe, 
     it, 
     expect,
-    vi,
  } from 'vitest'
 
-import { IPost } from '@/domain/models'
 import { makePost } from '@/main/usecases'
 
 describe('Use Case Post', () => {
@@ -21,82 +19,4 @@ describe('Use Case Post', () => {
             expect(typeof post).toBe('function')
         })
     })
-
-    describe('GetPosts', () => {
-        const post = makePost()
-
-        describe('Smoke Test', () => {
-
-            it('should return an getAll method', () => {
-                expect(post).toHaveProperty('getAll')
-            })
-
-            it('should check if getAll is a function', () => {
-                expect(typeof post.getAll).toBe('function')
-            })
-        })
-
-        describe('Status Code', () => {
-            
-            it('should return an status code 200 for Success', async () => {
-                const status = 200
-                const mock = { status }
-                post.getAll = vi.fn().mockResolvedValue(mock)
-                const response = await post.getAll()
-                expect(response.status).toEqual(mock.status)
-            })
-
-            it('should return an status code 400 for Bad request', async () => {
-                const status = 400
-                const mock = { status }
-                post.getAll = vi.fn().mockResolvedValue(mock)
-                const response = await post.getAll()
-                expect(response.status).toEqual(mock.status)
-            })
-
-            it('should return an status code 500 for Server Error', async () => {
-                const status = 500
-                const mock = { status }
-                post.getAll = vi.fn().mockResolvedValue(mock)
-                const response = await post.getAll()
-                expect(response.status).toEqual(mock.status)
-            })
-        })
-
-        describe('Data', () => {
-            
-            it('should return an empty array', async () => {
-                const data: IPost[] = []
-                const mock = { data }
-                post.getAll = vi.fn().mockResolvedValue(mock)
-                const response = await post.getAll()
-                expect(response.data).toEqual(mock.data)
-            })
-
-            it('should return an array of Post', async () => {
-                const data: IPost[] = [{
-                    userId: 1,
-                    id: 1,
-                    title: 'Post 1',
-                    body: ''
-
-                }]
-                const mock = { data }
-                post.getAll = vi.fn().mockResolvedValue(mock)
-                const response = await post.getAll()
-                expect(response.data).toEqual(mock.data)
-            })
-        })
-
-        describe('Error Handling', () => {
-
-            it('should throw an error', async () => {
-                post.getAll = vi.fn().mockImplementationOnce(() => {
-                    return Promise.reject(new Error())
-                })
-                await expect(post.getAll()).rejects.toThrow()
-            })
-        })
-    })
-
 })
