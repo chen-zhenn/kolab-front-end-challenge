860ffb65c04aaa8f7d01774e591c5e53dfe7c756
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-18 01:49:12 -0300

    feat(usecase): implement getAll for Post Use Case

--- code/frontend/kolab-feed/src/main/usecases/Post.ts
+++ code/frontend/kolab-feed/src/main/usecases/Post.ts
@@ -1,9 +1,27 @@
 import { IPost } from '../../domain/models'
+import { IHttpParams, IHttpResponse } from '../../infra/protocols'
+import { AxiosHttpClient } from '../../infra/http/axios/AxiosHttpClient'
+import { HttpClient } from '../../infra/http/HttpClient' 
+import { ServiceGetPost } from '../services'
 
 export default function makePost() {
 
-    async function getAll(): Promise<IPost[]> {
-        return await []
+    async function getAll(): Promise<IHttpResponse<IPost[]>> {
+
+        const params: IHttpParams = {
+            baseURL:'https://jsonplaceholder.typicode.com',
+            url: '/posts',
+        }
+
+        try {
+            const httpInstance = AxiosHttpClient.getInstance(params)
+            const httpClient = new HttpClient<IPost[]>(httpInstance.httpClient)
+    
+            const serviceGetPost = new ServiceGetPost(params, httpClient)
+            return await serviceGetPost.get() 
+        } catch (error) {
+            throw new Error(`${error}`)
+        }
     }
 
     return {
