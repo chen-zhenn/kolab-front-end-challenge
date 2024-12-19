a8132c50c45cf6e238e914a2942b8887a7dcf69c
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-19 04:43:14 -0300

    feat: update implementation of service and infra for handling query params

--- code/frontend/kolab-feed/src/infra/http/HttpClient.ts
+++ code/frontend/kolab-feed/src/infra/http/HttpClient.ts
@@ -4,14 +4,16 @@ import {
     IHttpParams
 } from '../protocols'
 
-export class HttpClient<T> implements IHttpClient<T> {
+import { Utils } from '../utils'
+
+export class HttpClient<T, Q> implements IHttpClient<T, Q> {
 
     constructor(private httpClient: any) {
         this.httpClient = httpClient
     }
 
-    async get(params: IHttpParams): Promise<IHttpResponse<T>> {
-        const url = `${params.baseURL}${params.url}`
+    async get(params: IHttpParams<Q>): Promise<IHttpResponse<T>> {
+        const url = this.buildURL(params)
         try {
             const response = await this.httpClient.get(url)
             return response 
@@ -19,5 +21,12 @@ export class HttpClient<T> implements IHttpClient<T> {
             console.log(error)
             throw new Error('Error fetching data')
         }
+    }
+
+    protected buildURL(params: IHttpParams<Q>){
+        const url =  params?.queryParams ? 
+            `${params.baseURL}${params.url}${Utils.serializeQueryParams(params.queryParams)}` : 
+            `${params.baseURL}${params.url}`
+        return url
     }
 }
