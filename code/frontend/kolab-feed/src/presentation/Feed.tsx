fe3d5733057c39c2c2c3c293568c21ab4341a5d4
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-19 18:33:52 -0300

    feat(view): integrate and update data on Feed Page

--- code/frontend/kolab-feed/src/presentation/Feed.tsx
+++ code/frontend/kolab-feed/src/presentation/Feed.tsx
@@ -1,6 +1,13 @@
 import { 
-    useLoaderData, 
-} from "react-router"
+    useLoaderData,
+    useNavigate, 
+} from 'react-router'
+
+import { 
+    Image,
+} from '@chakra-ui/react'
+
+import { Avatar } from '@/presentation/components/ui'
 
 import { 
     IPost, 
@@ -13,6 +20,7 @@ import {
 
 export default function Feed(){
 
+    const nav = useNavigate()
     const response: IHttpResponse<IPost[]> = useLoaderData()
     
     if (
@@ -21,9 +29,9 @@ export default function Feed(){
     ) return
     
     const posts: IPost[] = response.data
-
+    
     return (
-        <>
+        <div style={{ display: 'flex', flexFlow: 'column', gap: '1.75rem' }}>
             {
                 posts.map(post => (
                     <ul 
@@ -33,30 +41,51 @@ export default function Feed(){
                             display: 'flex',
                             flexFlow: 'column',
                             flexWrap: 'wrap',
+                            gap: '.85rem 0',
                             marginTop: 'unset', 
                             padding: '13px',
                             textAlign: 'left', 
-                            border: 'solid 1px rgba(0, 0, 0, 0.5)'
-                        }}>
+                            border: 'solid 1px rgba(0, 0, 0, 0.5)',
+                            cursor: 'pointer'
+                        }}
+                        onClick={() => nav(`/post/${post.userId}`)}
+                        >
+                        <li style={{ maxHeight: '230px', position: 'relative', display: 'flex', }}>
+                            <Image src={post.image} width='100%' fit='cover' />
+                        </li>
                         <li>
-                            <span style={{ fontWeight: 'bold' }}>Post Id: </span>
+                            <span style={{ fontWeight: 'bold' }}>Id do Post: </span>
                             <span>{ post.id }</span>
                         </li>                           
                         <li>
-                            <span style={{ fontWeight: 'bold' }}>Post Author Id: </span>
+                            <span style={{ fontWeight: 'bold' }}>Id do Autor: </span>
                             <span>{ post.userId }</span>
                         </li>
+                        <li style={{ display: 'flex', alignItems: 'center' }}>
+                            <Avatar name='John Doe' src={post.users?.avatar} />
+                            <span style={{ fontWeight: 'bold', marginLeft: '.85rem' }}>{post.users?.username}</span>
+                        </li>
                         <li>
-                            <span style={{ fontWeight: 'bold' }}>Post Title: </span>
+                            <span style={{ fontWeight: 'bold' }}>Titulo do Post: </span>
                             <span>{ post.title }</span>
                         </li>
                         <li>
-                            <span style={{ fontWeight: 'bold' }}>Post Content: </span>
+                            <span style={{ fontWeight: 'bold' }}>Conteúdo do Post: </span>
                             <span>{ post.body }</span>
+                        </li>
+                        <li>
+                            <span style={{ fontWeight: 'bold' }}>Comment: </span>
+                            {
+                                
+                                !!post.comments?.length && (
+                                    post.comments.map(comment => 
+                                        <span key={comment.id}>{comment.body}</span>)
+                                )
+                            }
                         </li>
                     </ul>
                 ))
             }
-        </>
+        </div>
     )
 }
--- code/frontend/kolab-feed/src/presentation/index.ts
+++ code/frontend/kolab-feed/src/presentation/index.ts
@@ -1,9 +1,7 @@
 import App from './App'
 import Feed from './Feed'
-import Post from './Post'
 
 export const View = {
     App,
     Feed,
-    Post,
 }
