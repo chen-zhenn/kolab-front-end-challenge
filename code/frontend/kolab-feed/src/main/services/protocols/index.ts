fd357b0bfbe1c7e975ebd5176f4bb0aadce6d58f
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-19 04:41:22 -0300

    feat: update interfaces of infra and services for handling query params

--- code/frontend/kolab-feed/src/main/services/protocols/index.ts
+++ code/frontend/kolab-feed/src/main/services/protocols/index.ts
@@ -1,3 +1,11 @@
+export interface IHttpPostQueryParams {
+    userId?: number;
+    id?: number;
+    title?: string;
+    body?: string;
+    q?: string;
+}
+
 export enum HttpStatusMessages {
     servererror = 'Erro interno: Problema ao processar sua solicitação. Tente novamente mais tarde',
     badrequest = 'Dados inválidos: Os parâmetros fornecidos na sua solicitação são inválidos.',
