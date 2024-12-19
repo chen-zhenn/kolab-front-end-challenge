500f77f2511e245eee64238d0f56e352ffaa32f5
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-19 10:22:30 -0300

    feat(layout): add TopBar component to layout

--- code/frontend/kolab-feed/src/presentation/layout/styles/index.tsx
+++ code/frontend/kolab-feed/src/presentation/layout/styles/index.tsx
@@ -10,6 +10,8 @@ export const Wrap = styled.div`
 export const Header = styled.header`
     grid-row: 1;
     grid-column: 2 / 3;
+    padding: 1.75rem;
+    background-color: rgba(255, 197, 32, 1);
 `
 
 export const Content = styled.main`
@@ -21,6 +23,8 @@ export const Content = styled.main`
 export const Sidebar = styled.aside`
     grid-row: 1 / 3;
     grid-column: 1 / 2;
+    color: rgba(255, 255, 255, 1);
+    background-color: rgba(26, 32, 44, 1);
 `
 
 export const Footer = styled.footer`
