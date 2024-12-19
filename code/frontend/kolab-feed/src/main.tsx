ee0625ce65e7eedfd1b6dfec38a024382b2b672a
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-19 10:15:27 -0300

    chore(ui): add chrakra provider

--- code/frontend/kolab-feed/src/main.tsx
+++ code/frontend/kolab-feed/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ChakraUIProvider } from '@/presentation/providers/chakra-ui'
 import { Router } from '@/routes/'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <ChakraUIProvider>
+      <Router />
+    </ChakraUIProvider>  
   </StrictMode>,
 )
