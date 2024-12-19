7d9770e3551365cb31aa3187c911096c788cce15
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-19 18:19:40 -0300

    feat: add and update model

--- code/frontend/kolab-feed/src/domain/models/Post.ts
+++ code/frontend/kolab-feed/src/domain/models/Post.ts
@@ -1,6 +1,13 @@
+import { IComments } from './Comments'
+import { IUsers } from './User'
+
 export interface IPost {
-    userId: number;
     id: number;
+    userId: number;
     title: string;
     body: string;
+    created_at?: string;
+    image?: string;
+    users?: IUsers;
+    comments?: IComments[];
 }
