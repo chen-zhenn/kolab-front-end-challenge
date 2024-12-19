4553892c7d3ca238e3c976fe6f8b6df897000070
Carlos Garcia <chenzhen.dev@gmail.com>
2024-12-19 10:13:42 -0300

    refactor(layout): delete includes components

--- code/frontend/kolab-feed/src/presentation/includes/Sidebar/types/index.ts
+++
@@ -1,3 +0,0 @@
-export interface ISidebar {
-    children: React.ReactNode;
-}
--- code/frontend/kolab-feed/src/presentation/includes/index.ts
+++
@@ -1,7 +0,0 @@
-import Header from './Header'
-import Sidebar from './Sidebar'
-
-export const Includes = {
-    Header,
-    Sidebar
-}
